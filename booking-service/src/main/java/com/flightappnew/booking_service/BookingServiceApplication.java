875653d8d086ecb6e623a14f8ac07b121fa2b9b7
Debarghya Sikder <debarghya1412@gmail.com>
2025-12-02 02:41:32 +0530

    Fixed the circuit breaker annotation

--- booking-service/src/main/java/com/flightappnew/booking_service/BookingServiceApplication.java
+++ booking-service/src/main/java/com/flightappnew/booking_service/BookingServiceApplication.java
@@ -3,11 +3,9 @@ package com.flightappnew.booking_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 
 @SpringBootApplication
 @EnableFeignClients(basePackages = "com.flightappnew.booking_service.client")
-@EnableCircuitBreaker
 public class BookingServiceApplication {
 
     public static void main(String[] args) {
